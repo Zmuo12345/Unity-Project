c46bf46a158ac398a2d36f14e3c0f5e5e50f1373
Zmuo12345 <Zmuo12345@gmail.com>
2016-09-22 18:06:41 +0700

    HexGrid 2.01

--- TM Project01/Assets/Scripts/HexGrid/HexCell.cs
+++ TM Project01/Assets/Scripts/HexGrid/HexCell.cs
@@ -5,4 +5,19 @@ public class HexCell : MonoBehaviour
 {
     public Color color;
     public HexCoordinates coordinates;
+
+    [SerializeField]
+    HexCell[] neighbors;
+
+
+    public HexCell GetNeighbor(HexDirection direction)
+    {
+        return neighbors[(int)direction];
+    }
+
+    public void SetNeighbor(HexDirection direction, HexCell cell)
+    {
+        neighbors[(int)direction] = cell;
+        cell.neighbors[(int)direction.Opposite()] = this;
+    }
 }
